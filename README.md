b23dd7c021b9b00a439ee89b0600f87ba58224a2
Niema Moshiri <niemamoshiri@gmail.com>
2024-04-24 19:58:37 +0000

    Added sequence subsampling

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Benchmark of cawlign and bealign
 
 ```bash
-./benchmark.py -s data/popset_183186851.fas.gz -n 1 -N 5 -d 1
+rm -rf benchmark_results.zip* && ./benchmark.py -s data/popset_183186851.fas.gz -n 1 -N 10 -d 1 -o benchmark_results.zip
 ```
--- benchmark.py
+++ benchmark.py
@@ -4,9 +4,36 @@ Benchmark cawlign and bealign. Assumes both are in your PATH.
 '''
 
 # imports
+from glob import glob
 from gzip import open as gopen
-from os.path import isfile
+from os import mkdir
+from os.path import isfile, isdir
+from random import choices
 import argparse
+
+# constants
+DEFAULT_REPS = 10
+OUTPUT_TMP_SUFFIX = '.tmp'
+
+# parse user args
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-s', '--sequences', required=True, type=str, help="Input Sequences (FASTA)")
+    parser.add_argument('-o', '--output', required=True, type=str, help="Output File (ZIP)")
+    parser.add_argument('-n', '--min_n', required=True, type=int, help="Minimum Number of Sequences in Benchmark (inclusive)")
+    parser.add_argument('-N', '--max_n', required=True, type=int, help="Maximum Number of Sequences in Benchmark (inclusive)")
+    parser.add_argument('-d', '--delta', required=True, type=int, help="Delta Between Subsequent Valus of n")
+    parser.add_argument('-r', '--reps', required=False, type=int, default=DEFAULT_REPS, help="Number of Replicates per n")
+    args = parser.parse_args()
+    assert isfile(args.sequences), "File not found: %s" % args.sequences
+    assert not isfile(args.output) and not isdir(args.output), "Output file exists: %s" % args.output
+    args.output_tmp = '%s%s' % (args.output, OUTPUT_TMP_SUFFIX)
+    assert not isdir(args.output_tmp) and not isfile(args.output_tmp), "Temporary output folder exists: %s" % args.output_tmp
+    assert args.min_n > 0, "min_n (%s) must be positive" % args.min_n
+    assert args.max_n > args.min_n, "max_n (%s) must be greater than min_n (%s)" % (args.max_n, args.min_n)
+    assert args.delta > 0, "delta (%s) must be positive" % args.delta
+    assert ((args.max_n - args.min_n) % args.delta) == 0, "max_n (%s) - min_n (%s) is not evenly divisible by delta (%s)" % (args.max_n, args.min_n, args.delta)
+    return args
 
 # read FASTA stream and return (ID,seq) dictionary
 def readFASTA(stream):
@@ -30,21 +57,17 @@ def readFASTA(stream):
     seqs[name] = seq
     return seqs
 
-# parse user args
-def parse_args():
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-s', '--sequences', required=True, type=str, help="Input Sequences (FASTA)")
-    parser.add_argument('-n', '--min_n', required=True, type=int, help="Minimum Number of Sequences in Benchmark (inclusive)")
-    parser.add_argument('-N', '--max_n', required=True, type=int, help="Maximum Number of Sequences in Benchmark (inclusive)")
-    parser.add_argument('-d', '--delta', required=True, type=int, help="Delta Between Subsequent Valus of n")
-    parser.add_argument('-r', '--reps', required=False, type=int, default=1, help="Number of Replicates per n")
-    args = parser.parse_args()
-    assert isfile(args.sequences), "File not found: %s" % args.sequences
-    assert args.min_n > 0, "min_n (%s) must be positive" % args.min_n
-    assert args.max_n > args.min_n, "max_n (%s) must be greater than min_n (%s)" % (args.max_n, args.min_n)
-    assert args.delta > 0, "delta (%s) must be positive" % args.delta
-    assert ((args.max_n - args.min_n) % args.delta) == 0, "max_n (%s) - min_n (%s) is not evenly divisible by delta (%s)" % (args.max_n, args.min_n, args.delta)
-    return args
+# create subsampled sequence dataset
+def subsample_seqs(seqs, n, out_fn):
+    assert n < len(seqs), "n (%s) is larger than the number of sequences (%s)" % (n, len(seqs))
+    IDs = choices(list(seqs.keys()), k=n)
+    if out_fn.lower().endswith('.gz'):
+        out_f = gopen(out_fn, 'wt')
+    else:
+        out_f = open(out_fn, 'w')
+    for ID in IDs:
+        out_f.write('>%s\n%s\n' % (ID, seqs[ID]))
+    out_f.close()
 
 # main program
 if __name__ == "__main__":
@@ -54,6 +77,22 @@ if __name__ == "__main__":
         seqs_f = gopen(args.sequences, 'rt')
     else:
         seqs_f = open(args.sequences, 'r')
-    seqs = readFASTA(seqs_f)
+    seqs = readFASTA(seqs_f); seqs_f.close()
     assert args.max_n <= len(seqs), "max_n (%s) is larger than the total number of sequences in file: %s" % (args.max_n, args.sequences)
-    pass # TODO subsample `seqs` with `reps` replicates for {min_n, min_n+delta, ..., max_n}
+
+    # create output tmp folder and benchmark datasets
+    mkdir(args.output_tmp)
+    len_max_n = len(str(args.max_n)); len_reps = len(str(args.reps))
+    for n in range(args.min_n, args.max_n+1, args.delta):
+        n_s = str(n).zfill(len_max_n)
+        for r in range(1, args.reps+1):
+            r_s = str(r).zfill(len_reps)
+            curr_rep_name = 'n%s.r%s' % (n_s, r_s)
+            curr_rep_folder = '%s/%s' % (args.output_tmp, curr_rep_name)
+            mkdir(curr_rep_folder)
+            curr_rep_fasta_path = '%s/%s.fas' % (curr_rep_folder, curr_rep_name)
+            subsample_seqs(seqs, n, curr_rep_fasta_path)
+
+    # run bealign and cawlign on each dataset
+    for seq_fn in sorted(glob('%s/n*.r*/*.fas' % args.output_tmp)):
+        print(seq_fn) # TODO
